faa6284446928ce715dedb0e9d45ae8336c8e77a
Hendra Wardana <maxnara19@gmail.com>
2021-05-22 02:15:07 +0700

    add view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, viewsets, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from .usecases import SessionUseCase
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class SessionViewSet(viewsets.ViewSet):
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def create(request: Request):
+        session = SessionUseCase(request.user)
+        data = session.create()
+
+        return Response(data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def join(request: Request, session_code: str):
+        session = SessionUseCase(request.user)
+        data = session.join(session_code)
+
+        return Response({"message": data})
+
